1a72556e60c1edc2cea954950e409e368705ce30
vytre <vegaytre@gmail.com>
2022-05-26 18:58:49 +0200

    k

--- lessons/src/main/java/eksamenPrep/SecondTry/h2021/Ex04.java
+++ lessons/src/main/java/eksamenPrep/SecondTry/h2021/Ex04.java
@@ -1,4 +1,69 @@
 package eksamenPrep.SecondTry.h2021;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Ex04 {
+
+    public Double pipe1(String course,List<Student> students){
+
+        return students.stream()
+                .flatMap(student -> student.getExamPoints().entrySet().stream())
+                .filter(stringDoubleEntry -> course.equals(stringDoubleEntry.getKey()))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.averagingDouble(value -> value));
+
+    }
+
+    public List<Student> pipe2(List<Course> courses){
+        return courses.stream()
+                .filter(course -> course.getEvaluation() != null)
+                .filter(course -> course.toipics.contains("programming"))
+                .flatMap(course -> course.getPoints().keySet().stream())
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    public static class Student{
+        public int student_id;
+        public String name;
+        public Map<String,Double> examPoints;
+
+        public int getStudent_id() {
+            return student_id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Map<String, Double> getExamPoints() {
+            return examPoints;
+        }
+    }
+
+    public static class Course{
+        public String course_code;
+        public String toipics;
+        public String evaluation;
+        public Map<Student,Integer> points;
+
+        public String getCourse_code() {
+            return course_code;
+        }
+
+        public String getToipics() {
+            return toipics;
+        }
+
+        public String getEvaluation() {
+            return evaluation;
+        }
+
+        public Map<Student, Integer> getPoints() {
+            return points;
+        }
+    }
+
 }
---
+++ lessons/src/test/java/EksamenPrep/SecondTry/h2021/Ex04Test.java
@@ -0,0 +1,16 @@
+package EksamenPrep.SecondTry.h2021;
+
+import eksamenPrep.SecondTry.h2021.Ex04;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ex04Test {
+
+    @Test
+    public void vegardTest1(){
+        Ex04 ex04 = new Ex04();
+
+
+    }
+}
