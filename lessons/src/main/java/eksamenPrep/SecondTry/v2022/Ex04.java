70164c3d77e60081d864fa749926f6af44228580
vytre <vegaytre@gmail.com>
2022-05-28 05:03:52 +0200

    Update Ex04.java

--- lessons/src/main/java/eksamenPrep/SecondTry/v2022/Ex04.java
+++ lessons/src/main/java/eksamenPrep/SecondTry/v2022/Ex04.java
@@ -1,9 +1,7 @@
 package eksamenPrep.SecondTry.v2022;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.lang.reflect.Array;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Ex04 {
