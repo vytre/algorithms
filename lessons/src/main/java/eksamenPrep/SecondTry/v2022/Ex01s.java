361de21cf30895f71c00ccca991e52b913327bef
vytre <vegaytre@gmail.com>
2022-06-02 22:23:20 +0200

    j

--- lessons/src/main/java/eksamenPrep/SecondTry/v2022/Ex01s.java
+++ lessons/src/main/java/eksamenPrep/SecondTry/v2022/Ex01s.java
@@ -2,4 +2,16 @@ package eksamenPrep.SecondTry.v2022;
 
 public class Ex01s {
 
+    public String regexA(){
+        return "Is this an out of season april fools joke[\\?|\\!]";
+    }
+
+    public String regexB(){
+        return "[Ii][Ss]( )+[Tt][Hh][Ii][Ss]( )+[Aa][Nn]( )+[Oo][Uu][Tt]( )+[Oo][Ff]( )+[Ss][Ee][Aa][Ss][Oo][Nn]( )+[Aa][Pp][Rr][Ii][Ll]( )+[Ff][Oo][Oo][Ll][Ss]( )*[Jj][Oo][Kk][Ee]?";
+    }
+
+    public String regexC(){
+
+    }
+
 }
