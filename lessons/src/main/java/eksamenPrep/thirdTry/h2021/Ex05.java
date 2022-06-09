5e4eaefae0a07cc4fc657094ad649a41eeaed0ae
vytre <vegaytre@gmail.com>
2022-06-09 21:34:19 +0200

    s

---
+++ lessons/src/main/java/eksamenPrep/thirdTry/h2021/Ex05.java
@@ -0,0 +1,20 @@
+package eksamenPrep.thirdTry.h2021;
+
+import org.pg4200.les11.BitWriter;
+
+public class Ex05 {
+
+    public byte[] compress(String move){
+
+    }
+
+    public byte[] deCompress(String move){
+
+        BitWriter bitWriter = new BitWriter();
+        String chessMove = "";
+
+
+
+    }
+
+}
